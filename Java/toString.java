f789c5cf637a1ee710c6a4883e723504ee7c6dbf
EenMacFullstack <Iain.mcdulling@gmail.com>
2022-11-07 23:59:35 +0000

    iterate3

--- Java/toString.java
+++ Java/toString.java
@@ -2,4 +2,10 @@ public class toString {
     public static String smash(String... words) {
         return String.join("", words);
     }
+
 }
+
+int [] array = ["1", "2", "3"];
+
+
+system.out.prntln(smash(array))
---
+++ Javascript/Iterate3.js
@@ -0,0 +1,6 @@
+function plusOne(params) {
+    var number = params += 1
+    return number
+}
+
+console.log(plusOne(1));
